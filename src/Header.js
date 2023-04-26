ac0bebf556a7a5a7a785ed80d9204e01f5689b4f
Hasan <hasanahmed987123@gmail.com>
2023-04-27 01:28:57 +0500

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header_left'></div>
+      <div className='header_right'></div>
+    </div>
+  )
+}
+
+export default Header
