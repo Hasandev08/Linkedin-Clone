813861fae1af813e5e8b716227737c3d001dcd29
Hasan <hasanahmed987123@gmail.com>
2023-04-28 02:25:26 +0500

    Header.js (In Progress)

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search'
 
 function Header() {
   return (
     <div className='header'>
-      <div className='header_left'></div>
+      <div className='header_left'>
+        <img src='https://www.flaticon.com/free-icons/linkedin' alt='' />
+
+        <div className='header_search'>
+          <SearchIcon />
+          <input type='text' />
+        </div>
+      </div>
       <div className='header_right'></div>
     </div>
   )
